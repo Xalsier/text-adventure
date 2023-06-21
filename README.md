ed131551ae1a920942ed5e603a5bf109a37ff7e7
Xalsier <immortalfawx@gmail.com>
2023-06-20 21:21:19 -0400

    Fixed color command

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 help: "Here are your commands:
-  <br/>start - Begin your adventure
-  <br/>exit - Exit the adventure
+  <br/>start - Begin your adventure // Unfinished
+  <br/>exit - Exit the adventure // Working
   <br/>path - View your decision path! // Broken
-  <br/>color 'text' - Change console text color // Broken
+  <br/>color 'text' - Change console text color // Fixed
   <br/>"
--- app.js
+++ app.js
@@ -9,63 +9,77 @@ let svg = d3.select("svg"),
     height = +svg.attr("height"),
     g = svg.append("g").attr("transform", "translate(40,0)");
 
-Promise.all([
-  fetch('cmds.yaml')
-    .then(response => response.text())
-    .then(data => {
-      commands = jsyaml.safeLoad(data);
-    }),
-  fetch('tree.yaml')
-    .then(response => response.text())
-    .then(data => {
-      treeData = jsyaml.safeLoad(data);
-    }),
-]).then(() => {
-  consoleDiv.innerHTML += commands['help']; // Display help on start
-  input.addEventListener("keydown", function(e) {
-    if (e.key === "Enter") {
-      e.preventDefault();
-      let inputVal = input.value;
-      consoleDiv.innerHTML += "> " + inputVal + "<br/>";
-      if (inputVal in commands) {
-        switch (inputVal) {
-          case 'start':
-            currentId = "0";
-            path = [currentId];  // Start a new path
-            consoleDiv.innerHTML += treeData[currentId].text + "<br/>";
-            break;
-          case 'clear':
-            consoleDiv.innerHTML = "";
-            break;
-          case 'path':
-            let canvas = document.querySelector("#canvas");
-            if (getComputedStyle(canvas).display === "none") {
-              consoleDiv.innerHTML += path.join(' -> ') + "<br/>";  // Print out the path
-              canvas.style.display = "block";
-              updateTree(); // Update tree when showing it
-            } else {
-              canvas.style.display = "none";
-              svg.selectAll("*").remove(); // Clear the existing tree when hiding the canvas
+    Promise.all([
+      fetch('cmds.yaml')
+        .then(response => response.text())
+        .then(data => {
+          commands = jsyaml.safeLoad(data);
+        }),
+      fetch('tree.yaml')
+        .then(response => response.text())
+        .then(data => {
+          treeData = jsyaml.safeLoad(data);
+        }),
+    ]).then(() => {
+      consoleDiv.innerHTML += commands['help']; // Display help on start
+      input.addEventListener("keydown", function(e) {
+        if (e.code === "Enter") {
+          e.preventDefault();
+          let inputVal = input.value.trim();
+          
+          // Add input to console log
+          consoleDiv.innerHTML += "> " + inputVal + "<br/>";
+          const { command, args } = parseCommandAndArgs(inputVal);
+          // Check if input matches a command
+          if (command in commands) {
+            switch (commands[command]) {
+              case 'start':
+                currentId = "0";
+                path = [currentId];
+                consoleDiv.innerHTML += treeData[currentId].text + "<br/>";
+                break;
+              case 'exit':
+                consoleDiv.innerHTML = "";
+                break;
+              case 'path':
+                let canvas = document.querySelector("#canvas");
+                if (getComputedStyle(canvas).display === "none") {
+                  consoleDiv.innerHTML += "Showing path...<br/>";
+                  canvas.style.display = "block";
+                  svg.selectAll("*").remove();
+                  updateTree(); // Update tree when showing it
+                } else {
+                  consoleDiv.innerHTML += "Hiding path...<br/>";
+                  canvas.style.display = "none";
+                  svg.selectAll("*").remove();
+                }
+                break;
+                case 'color':
+                  changeConsoleTextColor(args);
+                  break;
+              default:
+                consoleDiv.innerHTML += "Unknown command. Try again.<br/>";
             }
-            break;
-          case 'color':
-            changeConsoleTextColor(inputVal);
-            break;
-          default:
-            consoleDiv.innerHTML += commands[inputVal]; // Print command result
+          } else if (inputVal === "1" || inputVal === "2") {
+            if (treeData[currentId]["option_" + inputVal]) {
+              currentId = treeData[currentId]["option_" + inputVal];
+              path.push(currentId);
+              consoleDiv.innerHTML += treeData[currentId].text + "<br/>";
+            } else {
+              consoleDiv.innerHTML += "Invalid option.<br/>";
+            }
+          } else {
+            consoleDiv.innerHTML += "Unknown command or option. Try again.<br/>";
+          }
+          
+          // Clear the input field
+          input.value = "";
         }
-      } else if (inputVal === "1" || inputVal === "2") {
-        currentId = treeData[currentId]["option_" + inputVal];
-        path.push(currentId);  // Add the new node to the path
-        consoleDiv.innerHTML += treeData[currentId].text + "<br/>";
-        svg.selectAll("*").remove();
-        updateTree();
-      }
-      input.value = "";
-    }
-  });
-  updateTree(); // Call updateTree here after loading data and setting up event listener
-});
+      });
+      
+      updateTree(); // Call updateTree here after loading data and setting up event listener
+    });
+    
 
 function updateTree() {
   let nodes = Object.keys(treeData).map(id => ({ id, ...treeData[id] }));
@@ -101,19 +115,22 @@ function updateTree() {
     .text(d => d.data.id);
 }
 
-function changeConsoleTextColor(inputVal) {
-    const colors = inputVal.split(" ");
-    if (colors.length === 2) {
-      const color = colors[1].trim().toLowerCase();
-      const consoleDiv = document.querySelector("#console");
-      const inputWrapper = document.querySelector(".input-wrapper");
-      const body = document.querySelector("body");
-  
-      consoleDiv.style.color = color;
-      inputWrapper.style.color = color;
-      body.style.color = color;
-    } else {
-      consoleDiv.innerHTML += "Invalid color command. Usage: 'color <color>'<br/>";
-    }
+function changeConsoleTextColor(color) {
+  if (color) {
+    const consoleDiv = document.querySelector("#console");
+    const inputWrapper = document.querySelector(".input-wrapper");
+    const body = document.querySelector("body");
+
+    consoleDiv.style.color = color;
+    inputWrapper.style.color = color;
+    body.style.color = color;
+  } else {
+    consoleDiv.innerHTML += "Invalid color command. Usage: 'color <color>'<br/>";
   }
-  
+}
+function parseCommandAndArgs(inputVal) {
+  const parts = inputVal.split(' ');
+  const command = parts.shift();
+  const args = parts.join(' ');
+  return { command, args };
+}
